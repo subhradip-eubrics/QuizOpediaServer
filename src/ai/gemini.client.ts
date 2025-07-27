9a286ab4bb2bfba18eee62eb058dae6f9f31eabd
subhradip-eubrics <subhradip@eubrics.com>
2025-07-28 00:15:50 +0530

    fix: gemini.client.js for env vars

--- src/ai/gemini.client.ts
+++ src/ai/gemini.client.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from '@google/genai';
 
 export const genAI = new GoogleGenAI({
-    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY || 'AIzaSyBnC8_IhsXK3xRlEarvpQaLKKOGrgcSKFU',
+    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY || '',
 });
 
 
